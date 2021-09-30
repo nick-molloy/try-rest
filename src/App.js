a97fbdab1936c1d7ee7b9a729cf036ce23f7b9e0
nick.molloy <nick@sproulmolloy.com>
2021-09-30 14:05:44 -0400

    dfszgxh

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import './App.css';
-
-
+import Axios from 'axios'
+import React, { useState } from 'react';
 
 const App = () =>{
-
- return (
-
- <div className="container">
-
- <p>nick-molloy</p>
-
- </div>
+    const [Joke, setJoke] = useState()
+    const getJoke = () => {
+        Axios.get('https://icanhazdadjoke.com/',{
+            headers: {
+              'Accept': 'application/json'
+            }
+          }
+        ).then(
+            (response) => {
+                console.log(response);
+                setJoke(response.data.joke);
+        }
+    );
+    };
+    return (
+<div className="container"><button onClick={getJoke}>Dad Joke</button>
+<br/><p>{Joke}</p>
+</div>
 
  );
 
 }
-
-
-
 export default App;
